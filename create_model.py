ab001152327d5a5216578117fd30b1dd64104bc7
Irvan Rizkiansyah <rizkiansyah.ir@gmail.com>
2021-05-28 19:58:35 +0700

    commit lagi

--- create_model.py
+++ create_model.py
@@ -45,3 +45,27 @@ def generate_features_and_labels():
     label_row_ids = label_row_ids.astype(np.int32, copy=False)
     onehot_labels = to_categorical(label_row_ids, len(label_uniq_ids))#ke one hot
     return np.stack(all_features), onehot_labels
+    
+features, labels = generate_features_and_labels()
+print(np.shape(features))
+print(np.shape(labels))
+
+training_split = 0.8
+
+alldata = np.column_stack((features, labels))
+
+np.random.shuffle(alldata)
+splitidx = int(len(alldata) * training_split)
+train, test = alldata[:splitidx,:], alldata[splitidx:,:]
+
+print(np.shape(train))
+print(np.shape(test))
+
+train_input = train[:,:-10]
+train_labels = train[:,-10:]
+
+test_input = test[:,:-10]
+test_labels = test[:,-10:]
+
+print(np.shape(train_input))
+print(np.shape(train_labels))
