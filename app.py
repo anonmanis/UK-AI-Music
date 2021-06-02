3eb3da51e645ca1a37c18dd3ab061e2d3902496e
Irvan Rizkiansyah <32705894+anonmanis@users.noreply.github.com>
2021-06-02 18:48:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File successfully uploaded', filename)
+            flash('File successfully uploadeds', filename)
             return redirect('/')
         else:
             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif, wav')
